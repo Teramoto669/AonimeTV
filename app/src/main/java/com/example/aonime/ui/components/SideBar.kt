2a40975d8bbdabbcf00dc147346ac636867dc425
Teramoto669 <rezhashahidzindarb@gmail.com>
2026-06-15 19:18:21 +0700

    update sidebar

--- app/src/main/java/com/example/aonime/ui/components/SideBar.kt
+++ app/src/main/java/com/example/aonime/ui/components/SideBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -101,6 +102,7 @@ fun SideBar(
     ) {
         Column(
             modifier = Modifier
+                .fillMaxWidth()
                 .fillMaxHeight()
                 .padding(vertical = 32.dp, horizontal = 8.dp)
                 .selectableGroup(),
